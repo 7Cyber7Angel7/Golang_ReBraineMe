aa7c5944d91a70c4afa0af5c2acc8c429aaaedca
7Cyber7Angel7 <87859372+7Cyber7Angel7@users.noreply.github.com>
2022-06-10 15:10:04 +0300

    Добавил текст который выводится при выполнении программы, стал понятней результат вывода

--- module02/Okruglenie/main.go
+++ module02/Okruglenie/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-      "fmt"
-      "math"
-    )
+	"fmt"
+	"math"
+)
 
-func main()  {
-  type AmericanVelocity float64
-  type EuropeanVelocity float64
+func main() {
+	type AmericanVelocity float64
+	type EuropeanVelocity float64
 
-  bike1 := 120.4      // скорость в м/с
-  bike2 := 130.0      // скорость в м/с
+	bike1 := 120.4 // скорость в м/с
+	bike2 := 130.0 // скорость в м/с
 
-  const km = 1000.0
-  const mile = 1609.0
+	const km = 1000.0
+	const mile = 1609.0
 
-  speed_bike1 := AmericanVelocity(convert(bike1, km))
-  speed_bike2 := EuropeanVelocity(convert(bike2, mile))
+	speed_bike1 := AmericanVelocity(convert(bike1, km))
+	speed_bike2 := EuropeanVelocity(convert(bike2, mile))
 
-fmt.Println(speed_bike1)
-fmt.Println(speed_bike2)
+	fmt.Printf("Байк номер 1 едет %v км/час \n", speed_bike1)
+	fmt.Printf("Байк номер 2 едет %v миль/час \n", speed_bike2)
 }
 
 // функция конвертации м/с в км/час   или   м/с в мили/час (результат округляется к двум знакам после запятой)
-func convert (s float64, v float64) float64 {
-   speed := (s * 3600) / v
-   result_speed := math.Round(speed * 100) / 100
-   return result_speed
+func convert(s float64, v float64) float64 {
+	speed := (s * 3600) / v
+	result_speed := math.Round(speed*100) / 100
+	return result_speed
 }
